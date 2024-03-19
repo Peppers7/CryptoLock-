3d5fc02a54a7518abb28d7e4b9f83f0e16bb8bfb
Matheus Paulino <matheusparrapaulino@gmail.com>
2024-03-19 16:19:50 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function alterarBackground(){
+  document.getElementById('alterar_background').style.background = "none";
+  let elementos = document.getElementsByClassName('remover_img');
+  for (let i = 0; i < 2; i++) {
+    elementos[i].style.display = "none";
+  }
+}
+
+function criptografarTexto(){
+  const acentosMinusculos = /[áàâãéèêíïóôõöúü]/;
+  const acentosMaiusculos = /[ÁÀÂÃÉÈÊÍÏÓÔÕÖÚÜ]/;
+  const selecionar = document.getElementById("entrada");
+  const retornar = document.getElementById("saida");
+  let entradacopiada = selecionar.value;
+  if(/[A-Z]/.test(entradacopiada) ||  acentosMinusculos.test(entradacopiada) || acentosMaiusculos.test(entradacopiada)){
+    alert("Apenas letras minusculas e NADA de acentos!!!");
+  }else{
+    alterarBackground()
+    entradacopiada = entradacopiada
+    .replaceAll(/e/g, "enter")
+    .replaceAll(/i/g, "imes")
+    .replaceAll(/a/g, "ai")
+    .replaceAll(/o/g, "ober")
+    .replaceAll(/u/g, "ufat");
+     
+    retornar.textContent = entradacopiada;
+  }
+}
+
+function copiarTexto(){
+  const selecionar = document.getElementById("saida").value;
+  navigator.clipboard.writeText(selecionar);
+}
+
+function descriptografar(){
+  const acentosMinusculos = /[áàâãéèêíïóôõöúü]/;
+  const acentosMaiusculos = /[ÁÀÂÃÉÈÊÍÏÓÔÕÖÚÜ]/;
+  const selecionar = document.getElementById("entrada");
+  const retornar = document.getElementById("saida");
+  let entradacopiada = selecionar.value;
+  if(/[A-Z]/.test(entradacopiada) ||  acentosMinusculos.test(entradacopiada) || acentosMaiusculos.test(entradacopiada)){
+    alert("Apenas letras minusculas e NADA de acentos!!!");
+  }else{
+    alterarBackground();
+    entradacopiada = entradacopiada
+    .replaceAll(/enter/g, "e")
+    .replaceAll(/imes/g, "i")
+    .replaceAll(/ai/g, "a")
+    .replaceAll(/ober/g, "o")
+    .replaceAll(/ufat/g, "u");
+     
+    retornar.textContent = entradacopiada;
+   }
+}
